b1ec48d5750eb1a27bb0bb1a2ae7a25d918b4ae3
fukiran <macdorphoto@yahoo.com>
2021-02-23 20:11:41 +0000

    add models

--- src/main/java/com/codeclan/example/RelationshipHomework/models/Folder.java
+++ src/main/java/com/codeclan/example/RelationshipHomework/models/Folder.java
@@ -1,4 +1,52 @@
 package com.codeclan.example.RelationshipHomework.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Folder {
+
+    private String title;
+    private List<File>files;
+    private User user;
+    private Long id;
+
+    public Folder(String title, User user) {
+        this.title = title;
+        this.files = new ArrayList<>();
+        this.user = user;
+    }
+
+    public Folder(){}
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<File> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<File> files) {
+        this.files = files;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
--- src/main/java/com/codeclan/example/RelationshipHomework/models/User.java
+++ src/main/java/com/codeclan/example/RelationshipHomework/models/User.java
@@ -1,4 +1,41 @@
 package com.codeclan.example.RelationshipHomework.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+    private String name;
+    private Long id;
+    private List<Folder>folders;
+
+    public User(String name) {
+        this.name = name;
+        this.folders = new ArrayList<>();
+    }
+
+    public User(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Folder> getFolders() {
+        return folders;
+    }
+
+    public void setFolders(List<Folder> folders) {
+        this.folders = folders;
+    }
 }
